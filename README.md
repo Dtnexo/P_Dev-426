240b9919da8ab2445563e6026348be728dffeaf3
Bastien <po77qxd@eduvaud.ch>
2025-01-24 15:42:58 +0100

    commit de la base du site

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # P_Dev-426
+
+## how to run
+
+go to /P_DEV-426
+
+```
+npm install
+```
+
+and after
+
+```
+npm start
+```
+
+app run on port 3003
---
+++ src/app.mjs
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 3003;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+// Si aucune route ne correspondant à l'URL demandée par le consommateur
+// On place le code a la fin, car la requette passera d'abord par les autres route, et si aucune ne correspond la route n'est pas trouvé donc 404
+app.use(({ res }) => {
+  const message =
+    "Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.";
+  res.status(404).json(message);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
