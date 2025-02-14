9489b12c6f496424a82f279f0fde1a7749fa7ea6
Dtnexo <pt22ugm@eduvaud.ch>
2025-02-14 22:56:15 +0100

    front-end register

--- app/static/register.js
+++ app/static/register.js
@@ -1,16 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("Next").addEventListener("click", function () {
-    const page1 = document.getElementById("page1");
-    const page2 = document.getElementById("page2");
+  const page1 = document.getElementById("page1");
+  const page2 = document.getElementById("page2");
+  const nextBtn = document.getElementById("Next");
+  const prevBtn = document.querySelector(".Prev");
 
-    // Ajoutez les classes d'animation
+  nextBtn.addEventListener("click", function () {
+    // Enlever les anciennes animations
+    page1.classList.remove("slide-in-left", "slide-out-left");
+    page2.classList.remove("slide-in-right", "slide-out-right");
+
+    // Ajouter les nouvelles animations
     page1.classList.add("slide-out-left");
     page2.classList.add("slide-in-right");
 
-    // Changez l'affichage après l'animation
-    setTimeout(function () {
+    setTimeout(() => {
       page1.style.display = "none";
       page2.style.display = "flex";
-    }, 500); // La durée de l'animation est de 0.5s
+    }, 200);
+  });
+
+  prevBtn.addEventListener("click", function () {
+    console.log("Bouton Précédent cliqué !");
+    
+    // Enlever les anciennes animations
+    page1.classList.remove("slide-in-left", "slide-out-left");
+    page2.classList.remove("slide-in-right", "slide-out-right");
+
+    // Ajouter les nouvelles animations
+    page1.classList.add("slide-in-left");
+    page2.classList.add("slide-out-right");
+
+    setTimeout(() => {
+      page1.style.display = "flex";
+      page2.style.display = "none";
+    }, 200);
   });
 });
