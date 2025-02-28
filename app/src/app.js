b6953f8617c162d2091cbd8aeac1925a51334de0
romainschert <150144520+romainschert@users.noreply.github.com>
2025-02-28 15:22:56 +0100

    faire le header not connect

--- app/src/app.js
+++ app/src/app.js
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.get("/", (req, res) => {
-  res.redirect("/login");
+  res.redirect("/accueil");
 });
 
 app.use(
