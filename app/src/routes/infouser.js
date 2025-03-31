e5127211838f9205cd8b3b5c2344765908e84731
Evinne8 <pe41bnd@eduvaud.ch>
2025-03-31 14:10:56 +0200

    feat(infoUser): changer les info d'utilisateur [WIP]

--- app/src/routes/infouser.js
+++ app/src/routes/infouser.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { get } from "../controllers/infouserController.js";
+import { get, updateName } from "../controllers/infouserController.js"; // Assure-toi que updateName est bien exportée
 
 const infouserRouter = express.Router();
+
+// Route pour afficher la page
 infouserRouter.get("/", get);
+
+// Route API pour modifier le nom
+infouserRouter.post("/update-name", updateName);
 
 export { infouserRouter };
