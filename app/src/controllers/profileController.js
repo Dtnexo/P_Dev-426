0f196871f9e39249113bf0754e0f8bcacb9c92a4
Evinne8 <pe41bnd@eduvaud.ch>
2025-04-07 14:40:51 +0200

    feat(historique): Faire une requête sur la db pour récupérer l'historique d'un utilisateur

--- app/src/controllers/profileController.js
+++ app/src/controllers/profileController.js
@@ -1,6 +1,30 @@
 import "dotenv/config";
-const get = (req, res) => {
-  res.render("profile", { user: req.session.user || null });
+import { queryDatabase } from "../db/dbConnect.js";
+
+const get = async (req, res) => {
+  let historique;
+  try {
+    const user_id = await queryDatabase(
+      `SELECT user_id FROM t_user Where username='${req.session.user.username}'`
+    );
+    const historique = await queryDatabase(
+      `SELECT * FROM t_liste_favoris fav JOIN t_sites site ON site.site_id=fav.site_id WHERE fav.user_id='${user_id[0].user_id}'`
+    );
+    console.log(user_id[0].user_id);
+    console.log(historique);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des sites:", error);
+  }
+  const chartData = {
+    labels: ["dsaas", "dsdfaf", "eqwewqe", "dsadasd"],
+    values: [10, 20, 30, 40],
+  };
+
+  res.render("profile", {
+    user: req.session.user || null,
+    chartData: chartData,
+    historique: historique,
+  });
 };
 
 export { get };
