b3937b83e83a4cd13c8f5f92d0bf91d79b8bbdba
Evinne8 <pe41bnd@eduvaud.ch>
2025-05-05 14:27:45 +0200

    feat(pieChart): le camembert des continents fonctionnent
    [40MIN][DONE]

--- app/src/controllers/profileController.js
+++ app/src/controllers/profileController.js
@@ -9,23 +9,36 @@ const get = async (req, res) => {
     );
     console.log(name);
     req.session.user.username = name[0].username;
-    const historique = await queryDatabase(
+    const listFav = await queryDatabase(
       `SELECT * FROM t_avoir av JOIN t_sites si ON si.site_id = av.site_id JOIN t_liste_favoris fav ON fav.liste_favoris_id = av.liste_favoris_id WHERE fav.user_id = '${req.session.user.user_id}'`
     );
-    console.log(historique);
+    const historique = await queryDatabase(
+      `SELECT * FROM t_contenir co JOIN t_sites si ON si.site_id = co.site_id JOIN t_historique hist ON hist.historique_id = co.historique_id WHERE hist.user_id = '${req.session.user.user_id}'`
+    );
+    const totalHist = historique.length;
+    const counts = historique.reduce((acc, site) => {
+      acc[site.continent] = (acc[site.continent] || 0) + 1;
+      return acc;
+    }, {});
+    const result = Object.entries(counts).map(([continent, count]) => ({
+      continent,
+      count,
+      percentage: ((count / totalHist) * 100).toFixed(1),
+    }));
+    const chartData = {
+      labels: result.map((r) => r.continent + ` (${r.percentage}%)`),
+      values: result.map((r) => r.percentage), // valeurs numériques (sans le %)
+    };
+
+    res.render("profile", {
+      user: req.session.user || null,
+      chartData: chartData,
+      historique: historique,
+    });
+    console.table(result);
   } catch (error) {
     console.error("Erreur lors de la récupération des sites:", error);
   }
-  const chartData = {
-    labels: ["dsaas", "dsdfaf", "eqwewqe", "dsadasd"],
-    values: [10, 20, 30, 40],
-  };
-
-  res.render("profile", {
-    user: req.session.user || null,
-    chartData: chartData,
-    historique: historique,
-  });
 };
 
 export { get };
