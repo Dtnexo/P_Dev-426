81bbe3fb68aa97982c720de6a051424f6188c913
Dtnexo <pt22ugm@eduvaud.ch>
2025-05-05 13:57:10 +0200

    feat(infouser) username update fix

--- app/src/controllers/profileController.js
+++ app/src/controllers/profileController.js
@@ -10,7 +10,7 @@ const get = async (req, res) => {
     console.log(name);
     req.session.user.username = name[0].username;
     const historique = await queryDatabase(
-      `SELECT * FROM t_liste_favoris fav JOIN t_sites site ON site.site_id=fav.site_id WHERE fav.user_id='${user_id}'`
+      `SELECT * FROM t_liste_favoris fav JOIN t_sites site ON site.site_id=fav.site_id WHERE fav.user_id='${req.session.user.user_id}'`
     );
     console.log(historique);
   } catch (error) {
