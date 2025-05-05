10abac813db9c6c5ba3c04044dc8980924057595
Bastien <po77qxd@eduvaud.ch>
2025-05-05 14:06:09 +0200

    feat(camembert): ajout du camembert
    [WIP], encore des bugs

--- app/src/controllers/profileController.js
+++ app/src/controllers/profileController.js
@@ -3,21 +3,25 @@ import { queryDatabase } from "../db/dbConnect.js";
 
 const get = async (req, res) => {
   let historique;
+  let response;
   try {
     const user_id = await queryDatabase(
-      `SELECT user_id FROM t_user Where username='${req.session.user.username}'`
+      `SELECT user_id FROM t_user WHERE username='${req.session.user.username}'`
     );
     const historique = await queryDatabase(
       `SELECT * FROM t_liste_favoris fav JOIN t_sites site ON site.site_id=fav.site_id WHERE fav.user_id='${user_id[0].user_id}'`
     );
-    console.log(user_id[0].user_id);
-    console.log(historique);
+    response = await fetch(
+      "http://localhost:3003/api/continentWheel?user_id=" + user_id[0].user_id
+    );
   } catch (error) {
     console.error("Erreur lors de la récupération des sites:", error);
   }
+
+  const rawData = await response.json();
   const chartData = {
-    labels: ["dsaas", "dsdfaf", "eqwewqe", "dsadasd"],
-    values: [10, 20, 30, 40],
+    labels: rawData.map((item) => item.continent),
+    values: rawData.map((item) => item.nombre_sites),
   };
 
   res.render("profile", {
--- app/static/js/Chart.js
+++ app/static/js/Chart.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const pieCanvas = document.getElementById("pieCanvas");
   const data = window.chartDataFromServer;
+  const user = window.userFromServer;
 
   if (!pieCanvas || !data) {
     console.error("Canvas ou données manquantes !");
@@ -17,6 +18,30 @@ document.addEventListener("DOMContentLoaded", () => {
           backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#4BC0C0"],
         },
       ],
+    },
+    options: {
+      onClick: async (event, elements, chart) => {
+        if (elements[0]) {
+          const i = elements[0].index;
+          //alert(chart.data.labels[i] + ": " + chart.data.datasets[0].data[i]);
+          //modifier les données du chart
+          //pieChart.data.labels = ["dsaas", "dsdfaf", "eqwewqe", "dsadasd"];
+          //pieChart.data.datasets[0].data = [10, 20, 30, 40];
+          let response = await fetch(
+            "http://localhost:3003/api/countryWheel?user_id=" +
+              user.user_id +
+              "&continent=" +
+              chart.data.labels[i]
+          );
+          const rawData = await response.json();
+          console.log(rawData);
+          (pieChart.data.labels = rawData.map((item) => item.states)),
+            (pieChart.data.datasets[0].data = rawData.map(
+              (item) => item.nombre_sites
+            )),
+            pieChart.update();
+        }
+      },
     },
   });
 });
