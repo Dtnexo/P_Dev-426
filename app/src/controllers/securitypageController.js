7cafa4ffee22028c659ca4f80f698f2eece6ac7d
romainschert <150144520+romainschert@users.noreply.github.com>
2025-05-12 13:12:14 +0200

    Delete(securityPage): supprimer le code superflue dans la page sécurité

--- app/src/controllers/securitypageController.js
+++ app/src/controllers/securitypageController.js
@@ -6,42 +6,6 @@ const get = (req, res) => {
   res.render("securitypage", { user: req.session.user || null });
 };
 
-// Mise à jour du nom
-const updateName = async (req, res) => {
-  try {
-    console.log("BODY REÇU :", req.body);
-    const { id, name } = req.body;
-
-    if (!id || !name) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Champs manquants" });
-    }
-
-    const result = await queryDatabase(
-      "UPDATE t_user SET username = ? WHERE user_id = ?",
-      [name, id]
-    );
-    console.log("Résultat SQL:", result);
-
-    if (result.affectedRows === 0) {
-      return res
-        .status(404)
-        .json({ success: false, error: "Utilisateur non trouvé" });
-    }
-
-    // Met à jour la session si besoin
-    if (req.session.user && req.session.user.user_id === id) {
-      req.session.user.username = name;
-    }
-
-    return res.json({ success: true });
-  } catch (error) {
-    console.error("Erreur updateName:", error);
-    return res.status(500).json({ success: false, error: "Erreur serveur" });
-  }
-};
-
 const update2FA = async (req, res) => {
   const { user_id, enable_2fa } = req.body;
 
